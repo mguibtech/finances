ce069621376cf04c01437bfd6037c562b6bd2fee
mguibtech <mguibtech@gmail.com>
2023-02-15 04:54:14 -0300

    Building initial structure for the project

--- App.tsx
+++ App.tsx
@@ -1,21 +1,14 @@
-import { StatusBar } from 'expo-status-bar';
+
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import { Welcome } from './src/components/Welcome';
+import { Dashboard } from './src/screens/Dashboard';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Welcome title='Bare workflow com typeScript'/>
-    </View>
+
+    <Dashboard />
+
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
