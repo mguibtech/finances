ff9cb4e6ac7cc1a4b16296171689b4ad463b3f86
mguibtech <mguibtech@gmail.com>
2023-02-17 03:43:21 -0300

    Creatting theme of application using styled-components

---
+++ src/screens/Dashboard/index.tsx
@@ -0,0 +1,9 @@
+import { Container, Title } from "./styles";
+
+export function Dashboard(){
+    return(
+        <Container>
+            <Title>Testando aqui</Title>
+        </Container>
+    )
+}
